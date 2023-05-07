1fbca3d5ba98671131a7777c33bfd41cee3d47f2
Simon <horselvaren@gmail.com>
2023-05-07 16:30:17 +0600

    feat: complete 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,43 @@
+const fs = require('fs');
+const path = require('path');
 
+const folderPath =  path.join(__dirname, 'files')
+const folderCopyPath = path.join(__dirname, 'files-copy');
+
+const copyFiles = () => {
+  // Создание папки
+  fs.mkdir(folderCopyPath, { recursive: true }, (err) => {
+
+    // Перебор и копирование файлов
+    fs.readdir(folderPath, (err, files) => {
+      files.forEach(file => {
+        const sourceFile = path.join(__dirname, 'files', file);
+        const targetFile = path.join(__dirname, 'files-copy', file);
+
+        fs.readFile(sourceFile, (err, data) => {
+          if (err) throw err;
+
+          // Перенос содержимого файла
+          fs.writeFile(targetFile, data, (err) => {
+            if (err) throw err;
+          });
+        });
+      });
+    });
+  });
+}
+
+// Полное копирование папки
+fs.access(folderCopyPath, (err) => {
+  if (!err) {
+    
+    // Удалить копию папки, если она уже существует
+    fs.rmdir(folderCopyPath, { recursive: true }, (err) => {
+      copyFiles()
+    });
+    
+  } else {
+    // Копировать без удаления
+    copyFiles()
+  }
+});
